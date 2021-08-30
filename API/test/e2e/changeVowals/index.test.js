1be7f76fbe179292c79a926cce4a10302d190503
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-08-30 16:46:54 -0300

    update tests

--- API/test/e2e/changeVowals/index.test.js
+++ API/test/e2e/changeVowals/index.test.js
@@ -4,81 +4,91 @@ import { changeVowals } from '../testRoutes'
 
 describe('changeVowals route tests', () => {
   it('Should change the vowals correctly', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: 'It works',
       letter: 'c'
     })
 
     expect(body.result).toBe('ct wcrks')
+    expect(status).toBe(200)
   })
 
   it('Should res in an error with req.body without phrase property', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       letter: 'a'
     })
 
     expect(body.error).toBe('Phrase property is required.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with req.body without letter property', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: 'request with no letter property'
     })
 
     expect(body.error).toBe('Letter property is required.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with req.body empty JSON', async () => {
-    const { body } = await changeVowals({})
+    const { body, status } = await changeVowals({})
 
     expect(body.error).toBe('Phrase and letter properties are required.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with req.body without JSON', async () => {
-    const { body } = await changeVowals()
+    const { body, status } = await changeVowals()
 
     expect(body.error).toBe('JSON expected.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with req.body non JSON', async () => {
-    const { body } = await changeVowals('some random text')
+    const { body, status } = await changeVowals('some random text')
 
     expect(body.error).toBe('JSON expected.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with incorrect letter property', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: 'Request with incorrect letter property',
       letter: 'non letter'
     })
 
     expect(body.error).toBe('Letter property must be a character.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with empty letter property', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: 'Request with incorrect letter property',
       letter: '  '
     })
 
     expect(body.error).toBe('Letter property is required.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with empty phrase property', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: '  ',
       letter: ' a '
     })
 
     expect(body.error).toBe('Phrase property is required.')
+    expect(status).toBe(400)
   })
 
   it('Should res in an error with empty phrase and letter properties', async () => {
-    const { body } = await changeVowals({
+    const { body, status } = await changeVowals({
       phrase: '  ',
       letter: '  '
     })
 
     expect(body.error).toBe('Phrase and letter properties are required.')
+    expect(status).toBe(400)
   })
 })
--- API/test/e2e/removeDuplicate/index.test.js
+++ API/test/e2e/removeDuplicate/index.test.js
@@ -4,21 +4,24 @@ import { removeDuplicate } from '../testRoutes'
 
 describe('removeDuplicate route tests', () => {
   it('Should remove the duplicate words correctly', async () => {
-    const { body } = await removeDuplicate(
+    const { body, status } = await removeDuplicate(
       '  word Duplicate Word randomWord duplicate Duplicate Word word duplicate')
 
     expect(body.result).toBe('word Duplicate Word randomWord duplicate')
+    expect(status).toBe(200)
   })
 
   it('Should not remove words with a req.params without duplicates', async () => {
-    const { body } = await removeDuplicate('Word randomWord   duplicate word Duplicate')
+    const { body, status } = await removeDuplicate('Word randomWord   duplicate word Duplicate')
 
     expect(body.result).toBe('Word randomWord  duplicate word Duplicate')
+    expect(status).toBe(200)
   })
 
   it('Should res in an error with empty string in req.params', async () => {
-    const { body } = await removeDuplicate('%20%20') // URI encode for two whitespaces
+    const { body, status } = await removeDuplicate('%20%20') // URI encode for two whitespaces
 
     expect(body.error).toBe('Phrase param is required.')
+    expect(status).toBe(400)
   })
 })
